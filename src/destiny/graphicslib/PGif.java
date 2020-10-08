a1b2c3bf8b6e48579fb26aa90841ae86b6121997
Nathaniel-github <55164518+Nathaniel-github@users.noreply.github.com>
2020-10-08 00:21:18 -0700

    Added Library for rendering gifs
    I added a class that handles drawing gifs specifically for processing. It doesn't work perfectly so to say because I haven't handled any disposal methods that should be handled when drawing (I am not completely sure if there are any since many are handled when reading the gif to a BufferedImage)

---
+++ src/destiny/graphicslib/PGif.java
@@ -0,0 +1,188 @@
+package destiny.graphicslib;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class PGif {
+	
+	private ImageFrame[] frames;
+	private int frameCount;
+	private long lastTimeStamp;
+	private int x;
+	private int y;
+	private boolean firstDraw;
+	
+	public PGif(int x, int y, String pathName) {
+		
+		ImageReader reader = (ImageReader)ImageIO.getImageReadersByFormatName("gif").next();
+		
+		try {
+			reader.setInput(ImageIO.createImageInputStream(new File(pathName)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			frames = readGIF(reader);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		frameCount = 0;
+		
+	}
+	
+	public void draw(PApplet window) {
+		
+		if (!firstDraw) {
+			
+			int lastFrame = frameCount;
+			frameCount += (((System.nanoTime() - lastTimeStamp) / 10000000) / frames[frameCount].getDelay());
+			if (lastFrame != frameCount)
+				lastTimeStamp = System.nanoTime();
+			
+		} else {
+			
+			firstDraw = false;
+			lastTimeStamp = System.nanoTime();
+			
+		}
+		
+		if (frameCount >= frames.length || frameCount < 0) {
+			
+			frameCount = 0;
+			
+		}
+		
+		window.image(new PImage(frames[frameCount].getImage()), x, y);
+		
+	}
+	
+	private ImageFrame[] readGIF(ImageReader reader) throws IOException {
+	    ArrayList<ImageFrame> frames = new ArrayList<ImageFrame>(2);
+
+	    int width = -1;
+	    int height = -1;
+
+	    IIOMetadata metadata = reader.getStreamMetadata();
+	    if (metadata != null) {
+	        IIOMetadataNode globalRoot = (IIOMetadataNode) metadata.getAsTree(metadata.getNativeMetadataFormatName());
+
+	        NodeList globalScreenDescriptor = globalRoot.getElementsByTagName("LogicalScreenDescriptor");
+
+	        if (globalScreenDescriptor != null && globalScreenDescriptor.getLength() > 0) {
+	            IIOMetadataNode screenDescriptor = (IIOMetadataNode) globalScreenDescriptor.item(0);
+
+	            if (screenDescriptor != null) {
+	                width = Integer.parseInt(screenDescriptor.getAttribute("logicalScreenWidth"));
+	                height = Integer.parseInt(screenDescriptor.getAttribute("logicalScreenHeight"));
+	            }
+	        }
+	    }
+
+	    BufferedImage master = null;
+	    Graphics2D masterGraphics = null;
+
+	    for (int frameIndex = 0;; frameIndex++) {
+	        BufferedImage image;
+	        try {
+	            image = reader.read(frameIndex);
+	        } catch (IndexOutOfBoundsException io) {
+	            break;
+	        }
+
+	        if (width == -1 || height == -1) {
+	            width = image.getWidth();
+	            height = image.getHeight();
+	        }
+
+	        IIOMetadataNode root = (IIOMetadataNode) reader.getImageMetadata(frameIndex).getAsTree("javax_imageio_gif_image_1.0");
+	        IIOMetadataNode gce = (IIOMetadataNode) root.getElementsByTagName("GraphicControlExtension").item(0);
+	        int delay = Integer.valueOf(gce.getAttribute("delayTime"));
+	        String disposal = gce.getAttribute("disposalMethod");
+
+	        int x = 0;
+	        int y = 0;
+
+	        if (master == null) {
+	            master = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	            masterGraphics = master.createGraphics();
+	            masterGraphics.setBackground(new Color(0, 0, 0, 0));
+	        } else {
+	            NodeList children = root.getChildNodes();
+	            for (int nodeIndex = 0; nodeIndex < children.getLength(); nodeIndex++) {
+	                Node nodeItem = children.item(nodeIndex);
+	                if (nodeItem.getNodeName().equals("ImageDescriptor")) {
+	                    NamedNodeMap map = nodeItem.getAttributes();
+	                    x = Integer.valueOf(map.getNamedItem("imageLeftPosition").getNodeValue());
+	                    y = Integer.valueOf(map.getNamedItem("imageTopPosition").getNodeValue());
+	                }
+	            }
+	        }
+	        masterGraphics.drawImage(image, x, y, null);
+
+	        BufferedImage copy = new BufferedImage(master.getColorModel(), master.copyData(null), master.isAlphaPremultiplied(), null);
+	        frames.add(new ImageFrame(copy, delay, disposal));
+
+	        if (disposal.equals("restoreToPrevious")) {
+	            BufferedImage from = null;
+	            for (int i = frameIndex - 1; i >= 0; i--) {
+	                if (!frames.get(i).getDisposal().equals("restoreToPrevious") || frameIndex == 0) {
+	                    from = frames.get(i).getImage();
+	                    break;
+	                }
+	            }
+
+	            master = new BufferedImage(from.getColorModel(), from.copyData(null), from.isAlphaPremultiplied(), null);
+	            masterGraphics = master.createGraphics();
+	            masterGraphics.setBackground(new Color(0, 0, 0, 0));
+	        } else if (disposal.equals("restoreToBackgroundColor")) {
+	            masterGraphics.clearRect(x, y, image.getWidth(), image.getHeight());
+	        }
+	    }
+	    reader.dispose();
+
+	    return frames.toArray(new ImageFrame[frames.size()]);
+	}
+
+	private class ImageFrame {
+	    private final int delay;
+	    private final BufferedImage image;
+	    private final String disposal;
+
+	    public ImageFrame(BufferedImage image, int delay, String disposal) {
+	        this.image = image;
+	        this.delay = delay;
+	        this.disposal = disposal;
+	    }
+
+	    public BufferedImage getImage() {
+	        return image;
+	    }
+
+	    public int getDelay() {
+	        return delay;
+	    }
+
+	    public String getDisposal() {
+	        return disposal;
+	    }
+	}
+	
+}
--- src/destiny/panels/GraphicsPanel.java
+++ src/destiny/panels/GraphicsPanel.java
@@ -1,20 +1,25 @@
 package destiny.panels;
+
 import destiny.graphicslib.FadeVideo;
+import destiny.graphicslib.PGif;
 import processing.core.PApplet;
 import processing.video.Movie;
 
 public class GraphicsPanel extends PApplet {
 	
 	FadeVideo myMovie;
+	PGif test;
 
 	public void setup() {
 		myMovie = new FadeVideo(this, "res/titleScreen/Gurenge.mp4");
 		myMovie.loop();
+		test = new PGif(20, 20, "res/test.gif");
 	}
 
 	public void draw() {
 		
 		myMovie.draw(this);
+		test.draw(this);
 		
 	}
 	
