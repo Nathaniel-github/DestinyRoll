d863360e4aa13446293878166098e44abdf16244
Nathaniel-github <55164518+Nathaniel-github@users.noreply.github.com>
2021-01-31 20:53:15 -0800

    Add HomeScreen skeleton class

---
+++ src/destiny/panels/HomeScreen.java
@@ -0,0 +1,29 @@
+package destiny.panels;
+
+import destiny.core.Screen;
+import processing.core.PApplet;
+
+public class HomeScreen implements Screen {
+
+	@Override
+	public void setup(PApplet window) {
+		
+		
+
+	}
+
+	@Override
+	public void draw(PApplet window) {
+		
+		
+
+	}
+
+	@Override
+	public void dispose() {
+		
+		
+
+	}
+
+}
